bb405ebd227e2f3626f4802c86af9118c06168b6
Joseph Levarato <joseph@idusante.fr>
2022-05-14 11:33:10 +0200

    Updated Nuxt, added Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['nuxt-graphql-client'],
+    modules: [
+        'nuxt-graphql-client',
+        '@nuxtjs/tailwindcss',
+    ],
+
+    tailwindcss: {
+        viewer: false,
+        cssPath: '~/assets/css/tailwind.css'
+    }
 })
