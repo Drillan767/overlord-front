a5acd1346087f2760e879ffb501875dde193b731
Drillan767 <jd.levarato@gmail.com>
2025-07-19 16:41:30 +0200

    Improved latest articles, finished articles index and working on article detail

--- app/pages/articles/[...id].vue
+++ app/pages/articles/[...id].vue
@@ -1,19 +1,43 @@
 <script setup lang="ts">
 const route = useRoute()
 
-const { data: article } = await useAsyncData('article', () => queryCollection('articles')
+const { data: article } = await useAsyncData(route.path, () => queryCollection('articles')
     .where('path', '=', route.path)
     .first())
+
+useHead({
+    title: () => article.value?.title,
+    meta: [
+        {
+            name: 'description',
+            content: article.value?.seo?.description,
+        },
+    ],
+})
 </script>
 
 <template>
-    <ContentRenderer
-        v-if="article"
-        :value="article"
-    />
-    <div v-else>
-        <h1>
-            Article not found
-        </h1>
-    </div>
+    <VContainer fluid>
+        <VRow class="banner">
+            <VCol>
+                <h1>
+                    {{ article?.title }}
+                </h1>
+            </VCol>
+        </VRow>
+
+        <VRow>
+            <VCol>
+                <ContentRenderer
+                    v-if="article"
+                    :value="article"
+                />
+                <div v-else>
+                    <h1>
+                        Article not found
+                    </h1>
+                </div>
+            </VCol>
+        </VRow>
+    </VContainer>
 </template>
--- app/pages/articles/index.vue
+++ app/pages/articles/index.vue
@@ -1,9 +1,107 @@
 <script setup lang="ts">
+import Preview from '@/components/content/Preview.vue'
+
 useHead({
     title: 'Articles',
+})
+
+const route = useRoute()
+
+const subtitle = 'A collection of my thoughts and experiences.'
+
+const { data: articles } = await useAsyncData('articles', () => queryCollection('articles')
+    .order('date', 'DESC')
+    .select('id', 'title', 'description', 'image', 'date', 'tags', 'path')
+    .all())
+
+const activeTag = ref<string>()
+
+const tagsList = computed<string[]>(() => {
+    if (!articles.value)
+        return [] as string[]
+    return articles.value.reduce<string[]>((acc, article) => {
+        article.tags.forEach((tag) => {
+            if (!acc.includes(tag)) {
+                acc.push(tag)
+            }
+        })
+        return acc
+    }, [])
+})
+
+const filteredArticles = computed(() => {
+    if (!articles.value)
+        return []
+
+    return articles.value.filter(article => activeTag.value ? article.tags.includes(activeTag.value) : true)
+})
+
+onBeforeMount(() => {
+    activeTag.value = route.query.tag as string
 })
 </script>
 
 <template>
-    <h1>Articles</h1>
+    <VContainer>
+        <VRow>
+            <VCol>
+                <h1
+                    class="text-h2 text-center text-md-start layers glitch"
+                    data-text="Articles"
+                >
+                    Articles
+                </h1>
+                <p
+                    class="text-body-1 text-center text-md-start"
+                >
+                    {{ subtitle }}
+                </p>
+            </VCol>
+        </VRow>
+        <VRow>
+            <VCol>
+                <VCard
+                    color="primary"
+                    variant="tonal"
+                >
+                    <template #text>
+                        <div class="d-flex">
+                            <VChipGroup
+                                v-model="activeTag"
+                            >
+                                <VChip
+                                    v-for="tag in tagsList"
+                                    :key="tag"
+                                    :value="tag"
+                                    :text="tag"
+                                    :variant="activeTag === tag ? 'elevated' : 'outlined'"
+                                    color="primary"
+                                    rounded="lg"
+                                    class="mx-1 my-1"
+                                />
+                            </VChipGroup>
+                            <VBtn
+                                v-if="activeTag"
+                                icon="mdi-close"
+                                variant="text"
+                                color="primary"
+                                class="ml-auto"
+                                @click="activeTag = undefined"
+                            />
+                        </div>
+                    </template>
+                </VCard>
+            </VCol>
+        </VRow>
+        <VRow>
+            <VCol
+                v-for="article in filteredArticles"
+                :key="article.id"
+            >
+                <Preview
+                    :content="article"
+                />
+            </VCol>
+        </VRow>
+    </VContainer>
 </template>
